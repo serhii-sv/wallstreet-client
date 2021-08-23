3d24f57e4e2687c91f9b4c34fdf2b6c340b49d9f
fladko <vlad.babenko@tewris.com>
2021-08-23 07:00:49 +0300

    site disabling

---
+++ public/theme/css/errors/css.blade.php
@@ -0,0 +1,12 @@
+<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
+<!-- Plugins css start-->
+@yield('css')
+<!-- Plugins css Ends-->
+<!-- Bootstrap css-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
+<!-- App css-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+<link id="color" rel="stylesheet" href="{{asset('assets/css/color-1.css')}}" media="screen">
+<!-- Responsive css-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/responsive.css')}}">
