998db5d9c5d87e6b75c3c5845446b8d6ec6e5b20
Николаев Дмитрий Николаевич <fnxrus@gmail.com>
2021-09-20 14:58:04 +0300

    multi acc

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\UserAuthLog;
+use App\Models\UserMultiAccounts;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
@@ -45,6 +47,7 @@ class LoginController extends Controller
     {
         //
         $this->createUserAuthLog($request, $user);
+        $this->checkForMultiAccounts($request, $user);
     }
 
     public function logout(Request $request) {
@@ -76,10 +79,29 @@ class LoginController extends Controller
             'recaptchav3' => 'Captcha error! Try again',
         ]);
     }
-
+    public function checkForMultiAccounts(\Illuminate\Http\Request $request, $user) {
+        $current_ip = $request->ip();
+        $main_user = User::where('ip', $current_ip)->where('id', '!=', $user->id)->first();
+        $main_user_log = UserAuthLog::where('ip', $current_ip)->where('user_id', '!=', $user->id)->first();
+        if (!empty($main_user->isEmpty)) {
+            $this->createMultiAccountRecord($user, $main_user, $current_ip);
+        } else if (!empty($main_user_log)) {
+            $this->createMultiAccountRecord($user, $main_user_log->user_id, $current_ip);
+        }
+    }
+    
     public function username(){
         $field = (filter_var(request()->email, FILTER_VALIDATE_EMAIL) || !request()->email) ? 'email' : 'login';
         request()->merge([$field => request()->email]);
         return $field;
+    }
+    public function createMultiAccountRecord($user, $main_user, $ip) {
+        if(!(UserMultiAccounts::where('user_id', $user->id)->where('main_user_id', $main_user)->count() > 0 )){
+            $multi_acc = new UserMultiAccounts();
+            $multi_acc->user_id = $user->id;
+            $multi_acc->main_user_id = $main_user;
+            $multi_acc->ip = $ip;
+            $multi_acc->save();
+        }
     }
 }
