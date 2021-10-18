19bea65e33810baf0c5a97cb43894da3eb0777b5
Николаев Дмитрий Николаевич <fnxrus@gmail.com>
2021-10-18 16:30:38 +0300

    exchange

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -144,6 +144,37 @@ class Wallet extends Model
         Transaction::bonus($this, $amount);
         return $this;
     }
+    public function exchangeCurrency($wallet_from, $wallet_to, $amount, $commission = 0) {
+        if ($transaction_out = Transaction::exchangeOutCurrency($wallet_from, $amount)) {
+            $wallet_from->removeAmount($amount);
+        } else {
+            return null;
+        }
+        $toCurrency = Currency::where('code', 'USD')->first();
+        
+        $balance = $this->convertToCurrency($this->currency()->first(), $toCurrency, abs($amount)) - $commission; // Комиссия
+        $balance = $this->convertToCurrency($toCurrency, $wallet_to->currency()->first(), $balance);
+        
+        if ($transaction_in = Transaction::exchangeInCurrency($wallet_to, $balance)) {
+            $wallet_to->update(['balance' => $wallet_to->balance + $balance]);
+            $currency_exchange = new CurrencyExchange();
+            $currency_exchange->user_id = $wallet_from->user_id;
+            $currency_exchange->transaction_in = $transaction_in->id;
+            $currency_exchange->transaction_out = $transaction_out->id;
+            $currency_exchange->currency_in = $wallet_to->currency()->first()->id;
+            $currency_exchange->currency_out = $wallet_from->currency()->first()->id;
+            $currency_exchange->amount_in = $transaction_in->amount;
+            $currency_exchange->amount_out = $transaction_out->amount;
+            $currency_exchange->main_currency_amount_in = $transaction_in->main_currency_amount;
+            $currency_exchange->main_currency_amount_out = $transaction_out->main_currency_amount;
+            $currency_exchange->commission = 1;
+            if ($currency_exchange->save()) {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
 
     /**
      * @param $amount
