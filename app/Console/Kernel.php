6d3f05a6b8690bf1f0fe2ba9180542a4bccb6096
Serhii V <serhii.voronovych@gmail.com>
2021-10-24 13:08:03 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,14 +7,6 @@
 namespace App\Console;
 
 use App\Console\Commands\ChatServer;
-use App\Console\Commands\CheckPaymentSystemsConnectionsCommand;
-use App\Console\Commands\CreateAdminCommand;
-use App\Console\Commands\DepositQueueCommand;
-use App\Console\Commands\GenerateDemoDataCommand;
-use App\Console\Commands\CreateRootCommand;
-use App\Console\Commands\InstallScriptCommand;
-use App\Console\Commands\RegisterCurrenciesCommand;
-use App\Console\Commands\RegisterPaymentSystemsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -39,18 +31,6 @@ class Kernel extends ConsoleKernel
     {
         // Logs
         $schedule->command('log:clear')->daily()->withoutOverlapping();
-
-        // Jobs
-        $schedule->command('horizon:snapshot')->everyFiveMinutes();
-
-        // Financial
-        $schedule->command('check:payment_systems_connections')->everyTenMinutes()->withoutOverlapping();
-        $schedule->command('deposits:queue')->everyTenMinutes()->withoutOverlapping();
-        $schedule->command('update:currency_rates')->twiceDaily()->withoutOverlapping();
-
-        // Backups
-        $schedule->command('backup:clean')->hourly();
-        $schedule->command('backup:run', ['--only-db'])->hourly();
     }
 
     /**
