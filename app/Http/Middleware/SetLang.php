dfacfe43c9846c4b2d99b734ecd6056aede59b24
Serhii V <serhii.voronovych@gmail.com>
2021-10-30 14:48:15 +0800

    - update

--- app/Http/Middleware/SetLang.php
+++ app/Http/Middleware/SetLang.php
@@ -28,6 +28,12 @@ class SetLang
          */
         $defaultLang = 'ru';
 
+        if (!session()->has('lang')) {
+            session([
+                'lang' => $defaultLang,
+            ]);
+        }
+
         $path = resource_path('lang/' . $defaultLang . '.json');
 
         if (!file_exists($path)) {
@@ -37,7 +43,6 @@ class SetLang
         }
 
         if (isset($_COOKIE['lang']) && !session()->has('lang')) {
-            die('isset: '.$_COOKIE['lang']);
             $_COOKIE['lang']    = preg_replace('/[^A-Za-z]/', '', trim($_COOKIE['lang']));
             $checkExists        = file_exists(resource_path('lang/'.$_COOKIE['lang'].'.json'));
 
@@ -50,14 +55,12 @@ class SetLang
             }
         }
 
-        die($defaultLang.'-');
         $locale = session('lang', $defaultLang);
 
         if (!isset($_COOKIE['lang']) || $_COOKIE['lang'] != $locale) {
             setcookie('lang', $locale, Carbon::now()->addDays(365)->timestamp, '/');
         }
 
-        die($locale);
         App::setLocale($locale);
         Carbon::setLocale($locale);
 
