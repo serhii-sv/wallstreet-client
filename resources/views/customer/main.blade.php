e5015d959da91479245a3271a6eb3da719ab9750
Serhii V <serhii.voronovych@gmail.com>
2021-11-02 13:27:08 +0800

    - update

--- resources/views/customer/main.blade.php
+++ resources/views/customer/main.blade.php
@@ -969,6 +969,16 @@
                               @endif
 
                               @if(canEditLang() && checkRequestOnEdit())
+                                  <div style="text-align:left;">
+                                      <editor_block data-name='Add str 3 {{ $rate->id }}' contenteditable="true">{{ __('Add str 3 '.$rate->id) }}</editor_block>
+                                  </div>
+                              @else
+                                  <div style="text-align:left;">
+                                      <span  style="text-align:left;" class="date">{{ __('Add str 3 '.$rate->id) }}</span>
+                                  </div>
+                              @endif
+
+                              @if(canEditLang() && checkRequestOnEdit())
                                   @if($rate->overall)
                                       <editor_block style="text-align:left;" data-name='return deposit: true {{ $rate->id }}' contenteditable="true">{{ __('return deposit: true '.$rate->id) }}</editor_block>
                                   @else
