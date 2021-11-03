61d412dde647cd1566ef13183da374c3efccf6c8
Serhii V <serhii.voronovych@gmail.com>
2021-11-03 14:32:40 +0800

    update

--- resources/views/customer/agreement.blade.php
+++ resources/views/customer/agreement.blade.php
@@ -1,12 +1,63 @@
 @extends('layouts.app')
-@section('title', __('User agreement'))
+@section('title', __('Agreement title'))
 @section('content')
-    <main role="main" style="background: white">
-        <div class="page">
-            <svg class="before-svg" viewBox="0 0 1950 65.242325" height="65.242325" width="1950">
-                <path d="M 975,65.242324 H 0 V 35.889669 6.53701 L 21.25,5.88359 42.5,5.23016 88.000004,4.21053 133.5,3.19089 184,2.14419 234.5,1.09749 430.5,0.54874 626.5,0 l 85,1.14503 85,1.14503 46.5,0.96431 46.5,0.96431 41,1.0362 41,1.0362 49.5,1.48571 49.5,1.48571 42,1.50324 42,1.50325 48.5,1.99247 48.5,1.99247 43,2.013562 43,2.013565 38.5,2.004288 38.5,2.004288 43.5,2.486247 43.5,2.486246 47.5,3.01284 47.5,3.01284 36,2.496047 36,2.496046 13,0.97181 13,0.97181 40,3.03931 40,3.039311 48,3.987707 48,3.987708 38.4368,3.482384 38.4368,3.482385 h 1.3132 1.3132 v 1 1 z"></path>
-            </svg>
-            @include('partials.breadcrumbs')
+    <div class="main--body">
+        <!--========== Preloader ==========-->
+    {{--  @include('layouts.app-preloader')--}}
+    <!--========== Preloader ==========-->
+
+
+        <!--=======Header-Section Starts Here=======-->
+    @include('layouts.app-header')
+    <!--=======Header-Section Ends Here=======-->
+
+
+        <!--=======Banner-Section Starts Here=======-->
+        <section class="bg_img hero-section-2" data-background="{{ asset('theme/images/about/hero-bg3.jpg') }}">
+            <div class="container">
+                <div class="hero-content text-white">
+                    <h1 class="title">
+                        @if(canEditLang() && checkRequestOnEdit())
+                            <editor_block data-name='Agreement title 2' contenteditable="true">{{ __('Agreement title 2') }}</editor_block>
+                        @else
+                            {{ __('Agreement title 2') }}
+                        @endif
+                    </h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="{{ route('customer.main') }}" @if(canEditLang() && checkRequestOnEdit()) onclick="event.preventDefault()" @endif>@if(canEditLang() && checkRequestOnEdit())
+                                    <editor_block data-name='Home' contenteditable="true">{{ __('Home') }}</editor_block>
+                                @else
+                                    {{ __('Home') }}
+                                @endif</a>
+                        </li>
+                        <li>
+                            @if(canEditLang() && checkRequestOnEdit())
+                                <editor_block data-name='Agreement link' contenteditable="true">{{ __('Agreement link') }}</editor_block>
+                            @else
+                                {{ __('Agreement link') }}
+                            @endif
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="hero-shape">
+                <img class="wow slideInUp" src="{{ asset('theme/images/about/hero-shape1.png') }}" alt="about" data-wow-duration="1s">
+            </div>
+        </section>
+        <!--=======Banner-Section Ends Here=======-->
+
+
+        <!--=======Feature-Section Starts Here=======-->
+        <section class="faq-section padding-top padding-bottom mb-xl-5">
+            <div class="ball-group-1" data-paroller-factor="-0.30" data-paroller-factor-lg="0.60"
+                 data-paroller-type="foreground" data-paroller-direction="horizontal">
+                <img src="{{ asset('theme/images/balls/ball-group7.png') }}" alt="balls">
+            </div>
+            <div class="ball-group-2 rtl" data-paroller-factor="0.30" data-paroller-factor-lg="-0.30"
+                 data-paroller-type="foreground" data-paroller-direction="horizontal">
+                <img src="{{ asset('theme/images/balls/ball-group8.png') }}" alt="balls">
+            </div>
             <div class="container">
                 <h2 class="page-title page-title--line">
                     @if(canEditLang() && checkRequestOnEdit())
@@ -25,6 +76,14 @@
                     </p>
                 </div>
             </div>
-        </div>
-    </main>
+        </section>
+        <!--=======Feature-Section Ends Here=======-->
+
+        <!-- ==========Footer-Section Starts Here========== -->
+    @include('layouts.app-footer')
+    <!-- ==========Footer-Section Ends Here========== -->
+
+
+    </div>
+
 @endsection
