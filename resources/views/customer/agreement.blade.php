c67beb3846ecbfdc61d018718fa5edc5b71c2ab5
Serhii V <serhii.voronovych@gmail.com>
2021-11-03 14:28:36 +0800

    update

--- resources/views/customer/agreement.blade.php
+++ resources/views/customer/agreement.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.customer')
+@extends('layouts.app')
 @section('title', __('User agreement'))
 @section('content')
     <main role="main" style="background: white">
@@ -8,11 +8,20 @@
             </svg>
             @include('partials.breadcrumbs')
             <div class="container">
-                <h2 class="page-title page-title--line">User<span>Agreement</span>
+                <h2 class="page-title page-title--line">
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Agreement title' contenteditable="true">{{ __('Agreement title') }}</editor_block>
+                    @else
+                        {{ __('Agreement title') }}
+                    @endif
                 </h2>
                 <div class="text">
-                    <p><strong>Please read the following terms carefully before registering an account on our website.</strong></p>
-                    <p>You agree to be of legal age in your country to partake in this program, and in all the cases your minimal age must be 18 years. We are not available to the general public and is opened only to the qualified members of Luminex, the use of this site is restricted to our members and to individuals personally invited by them. Every deposit is considered to be a private transaction between the Luminex and its Member. As a private transaction, this program is exempt from the US Securities Act of 1933, the US Securities Exchange Act of 1934 and the US Investment Company Act of 1940 and all other rules, regulations and amendments thereof. We are not FDIC insured. We are not a licensed bank or a security firm. You agree that all information, communications, materials coming from Luminex are unsolicited and must be kept private, confidential and protected from any disclosure. Moreover, the information, communications and materials contained herein are not to be regarded as an offer, nor a solicitation for investments in any jurisdiction which deems non-public offers or solicitations unlawful, nor to any person to whom it will be unlawful to make such offer or solicitation. All the data given by a member to Luminex will be only privately used and not disclosed to any third parties. Luminex is not responsible or liable for any loss of data. You agree to hold all principals and members harmless of any liability. You are investing at your own risk and you agree that a past performance is not an explicit guarantee for the same future performance. You agree that all information, communications and materials you will find on this site are intended to be regarded as an informational and educational matter and not an investment advice. We reserve the right to change the rules, commissions and rates of the program at any time and at our sole discretion without notice, especially in order to respect the integrity and security of the members' interests. You agree that it is your sole responsibility to review the current terms. Luminex is not responsible or liable for any damages, losses and costs resulting from any violation of the conditions and terms and/or use of our website by a member. You guarantee to Luminex that you will not use this site in any illegal way and you agree to respect your local, national and international laws. Abuse of referral program is forbidden. All the involved accounts will be blocked. Don't post any bad votes on any public forums and without contacting Luminex support team first. Maybe there was a technical problem with your transaction, so please always make sure you get a reply from our support team. We will not tolerate SPAM or any type of UCE in this program. SPAM violators will be immediately and permanently removed from the program. Luminex reserves the right to accept or decline any member for membership without explanation. If you do not agree with the above disclaimer, please do not go any further.
+                    <p>
+                        @if(canEditLang() && checkRequestOnEdit())
+                            <editor_block data-name='Agreement text' contenteditable="true">{{ __('Agreement text') }}</editor_block>
+                        @else
+                            {{ __('Agreement text') }}
+                        @endif
                     </p>
                 </div>
             </div>
