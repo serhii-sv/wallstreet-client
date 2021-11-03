478e904adeea258da43e8eb875ff099b6ff4e214
Serhii V <serhii.voronovych@gmail.com>
2021-11-03 14:26:09 +0800

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,19 +14,12 @@
             @else
               {{ __('Profile') }}
             @endif</a>
-          @if(Auth::user()->isImpersonated())
-            <a class="btn btn--accent2 main-line__btn" @if(canEditLang() && checkRequestOnEdit()) onclick="event.preventDefault()" @endif href="{{ route('admin.impersonate.leave') }}">@if(canEditLang() && checkRequestOnEdit())
-                <editor_block data-name='Return to my account' contenteditable="true">{{ __('Return to my account') }}</editor_block>
-              @else
-                {{ __('Return to my account') }}
-              @endif</a>
-          @else
+
             <a class="btn btn--accent2 main-line__btn" @if(canEditLang() && checkRequestOnEdit()) onclick="event.preventDefault()" @else onclick="event.preventDefault(); document.getElementById('logout-form').submit();" @endif href="#">@if(canEditLang() && checkRequestOnEdit())
                 <editor_block data-name='Log out' contenteditable="true">{{ __('Log out') }}</editor_block>
               @else
                 {{ __('Log out') }}
               @endif</a>
-          @endif
         </div>
       @else
         <div class="main-line__buttons">
