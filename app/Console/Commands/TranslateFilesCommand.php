62f1ce595e966bb972c20d32ba18bf780660fa3e
Serhii V <serhii.voronovych@gmail.com>
2021-11-05 16:21:40 +0800

    update

---
+++ app/Console/Commands/TranslateFilesCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use JoggApp\GoogleTranslate\GoogleTranslate;
+use JoggApp\GoogleTranslate\GoogleTranslateClient;
+
+class TranslateFilesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'translate:files {from} {to}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Translate files automatically';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $from = $this->argument('from');
+        $to = $this->argument('to');
+
+        if (!Storage::disk('lang')->exists($from.'.json') || !Storage::disk('lang')->exists($from.'_manual.json')) {
+            $this->error('from files is not exists');
+            return Command::FAILURE;
+        }
+
+        $fromFile = Storage::disk('lang')->get($from.'.json');
+        $fromFileManual = Storage::disk('lang')->get($from.'_manual.json');
+
+        Storage::disk('lang')->put($from.'.backup.json', $fromFile);
+        Storage::disk('lang')->put($from.'_manual.backup.json', $fromFileManual);
+
+        $fromArray = json_decode($fromFile, true);
+        $fromManualArray = json_decode($fromFileManual, true);
+
+        // ---
+
+        $toArray = [];
+        $toManualArray = [];
+
+        $translate = new GoogleTranslate(new GoogleTranslateClient(config('googletranslate')));
+
+        foreach ($fromArray as $key => $val) {
+            $toArray[$key] = $translate->unlessLanguageIs($to, $val);
+            $this->info($val.' translated to '.$toArray[$key]);
+        }
+
+        foreach ($fromManualArray as $key => $val) {
+            $toManualArray[$key] = $translate->unlessLanguageIs($to, $val);
+            $this->info($val.' translated to '.$toArray[$key]);
+        }
+
+        // ---
+
+        $toArray = json_encode($toArray);
+        $toManualArray = json_encode($toManualArray);
+
+        Storage::disk('lang')->put($to.'.json', $toArray);
+        Storage::disk('lang')->put($to.'_manual.json', $toManualArray);
+
+        $this->info('success');
+
+        return Command::SUCCESS;
+    }
+}
