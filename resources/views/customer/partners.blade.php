e4e2f0cad682958c5cae5a8e696640c5ccf861b3
Serhii V <serhii.voronovych@gmail.com>
2021-11-05 13:40:08 +0800

    update

--- resources/views/customer/partners.blade.php
+++ resources/views/customer/partners.blade.php
@@ -419,7 +419,7 @@
     </section>
     <!-- ==========Total-Affiliate-Section Ends Here========== -->
 
-      <div class="row" style="margin:50px 0 50px 0;">
+      <div class="row" style="margin:50px 0 50px 0; text-align: center;">
           @if(canEditLang() && checkRequestOnEdit())
               <editor_block data-name='New descr 123' contenteditable="true">{{ __('New descr 123') }}</editor_block>
           @else
