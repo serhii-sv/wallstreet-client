97435074c5a2804418139bad23559ff2686ad03a
Serhii V <serhii.voronovych@gmail.com>
2021-11-05 13:53:03 +0800

    update

--- resources/views/customer/partners.blade.php
+++ resources/views/customer/partners.blade.php
@@ -419,7 +419,7 @@
     </section>
     <!-- ==========Total-Affiliate-Section Ends Here========== -->
 
-      <div class="row" style="margin:25px 0 25px 0; text-align: left;width:60%;margin-left:20%;">
+      <div class="row" style="margin:-10px 0 25px 0; text-align: left;width:60%;margin-left:20%;">
           @if(canEditLang() && checkRequestOnEdit())
               <editor_block data-name='New descr 123' contenteditable="true">{{ __('New descr 123') }}</editor_block>
           @else
@@ -642,7 +642,7 @@
                     {{ __('Join affiliate?') }}
                   @endif</h2>
 
-                  <div class="row" style="margin:-15px 0 25px 0; text-align: left;width:60%;margin-left:0%;">
+                  <div class="row" style="margin:25px 0 25px 0; text-align: left;width:60%;margin-left:0%;">
                       @if(canEditLang() && checkRequestOnEdit())
                           <editor_block data-name='New descr 1234' contenteditable="true">{{ __('New descr 1234') }}</editor_block>
                       @else
