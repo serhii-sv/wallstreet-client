2172adb2e456cca13784c1081c6d822415e71d91
Serhii V <serhii.voronovych@gmail.com>
2021-11-06 16:20:26 +0800

    update

--- resources/views/accountPanel/currency/exchange.blade.php
+++ resources/views/accountPanel/currency/exchange.blade.php
@@ -6,9 +6,9 @@ Exchange currency
 @section('content')
 
   <div class="container-fluid">
-    <div class="row second-chart-list third-news-update justify-content-center" style="margin-top:50px;">
-      <div class="card">
-        <div class="card-block row" style="margin-top:50px;">
+    <div class="row second-chart-list third-news-update justify-content-center">
+      <div class="card" style="margin-top:50px;">
+        <div class="card-block row">
           <div class="col-12">
             <div class="donut-chart-widget">
               <div class="card">
