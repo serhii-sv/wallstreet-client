d12871f79ba194071af825b91f4e4ac8c36c5e5d
Serhii V <serhii.voronovych@gmail.com>
2021-11-06 15:29:05 +0800

    update

--- resources/views/accountPanel/replenishment/manual.blade.php
+++ resources/views/accountPanel/replenishment/manual.blade.php
@@ -44,16 +44,16 @@ Topup balance details
                     @if(canEditLang() && checkRequestOnEdit())
                         <editor_block data-name='Replenishment text 6' contenteditable="true">{{ __('Replenishment text 6') }}</editor_block> @else {{ __('Replenishment text 6') }} @endif
                 </p>
-
-                <div class="f1-buttons" style="text-align: center;margin-top:50px;">
-                    <button class="btn btn-primary btn-previous" type="button" onClick="location.assign('https://t.me/sprintbank')"  style="padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
-                            <editor_block data-name='Previous 2' contenteditable="true">{{ __('Previous 2') }}</editor_block> @else {{ __('Previous 2') }} @endif
-                    </button>
-                    <button class="btn btn-primary btn-submit shake" id="next" onClick="location.assign('https://jivo.chat/xQE8U0bisX')" type="submit" data-bs-original-title="" title=""  style="margin-left:30px;padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
-                            <editor_block data-name='vnesti 2' contenteditable="true">{{ __('vnesti 2') }}</editor_block> @else {{ __('vnesti 2') }} @endif
-                    </button>
-                </div>
             </div>
+
+              <div class="f1-buttons" style="text-align: center;margin-top:50px;">
+                  <button class="btn btn-primary btn-previous" type="button" onClick="location.assign('https://t.me/sprintbank')"  style="padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
+                          <editor_block data-name='Previous 2' contenteditable="true">{{ __('Previous 2') }}</editor_block> @else {{ __('Previous 2') }} @endif
+                  </button>
+                  <button class="btn btn-primary btn-submit shake" id="next" onClick="location.assign('https://jivo.chat/xQE8U0bisX')" type="submit" data-bs-original-title="" title=""  style="margin-left:30px;padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
+                          <editor_block data-name='vnesti 2' contenteditable="true">{{ __('vnesti 2') }}</editor_block> @else {{ __('vnesti 2') }} @endif
+                  </button>
+              </div>
           </div>
         </div>
 
