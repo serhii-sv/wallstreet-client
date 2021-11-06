307fbfd83f835fd9c54cb13cca9d9577f1114d9e
Serhii V <serhii.voronovych@gmail.com>
2021-11-06 15:18:15 +0800

    update

--- resources/views/accountPanel/replenishment/manual.blade.php
+++ resources/views/accountPanel/replenishment/manual.blade.php
@@ -14,7 +14,7 @@ Topup balance details
               <h5>@if(canEditLang() && checkRequestOnEdit())
                   <editor_block data-name='Replenishment details' contenteditable="true">{{ __('Replenishment details') }}</editor_block> @else {{ __('Replenishment details') }} @endif {{ $paymentSystem !== null ? $paymentSystem->name : '' }}</h5>
             </div>
-            <div class="card-body" style="text-align: center;">
+            <div class="card-body" style="text-align: center; font-size:21px;">
               <p>
                 @if(canEditLang() && checkRequestOnEdit())
                   <editor_block data-name='Replenishment text 1' contenteditable="true">{{ __('Replenishment text 1') }}</editor_block> @else {{ __('Replenishment text 1') }} @endif
@@ -27,10 +27,15 @@ Topup balance details
                 @if(canEditLang() && checkRequestOnEdit())
                   <editor_block data-name='Replenishment text 2' contenteditable="true">{{ __('Replenishment text 2') }}</editor_block> @else {{ __('Replenishment text 2') }} @endif
               </p>
-              <p>
-                @if(canEditLang() && checkRequestOnEdit())
-                  <editor_block data-name='Replenishment text 3' contenteditable="true">{{ __('Replenishment text 3') }}</editor_block> @else {{ __('Replenishment text 3') }} @endif
-              </p>
+
+                <div class="f1-buttons" style="text-align: center;margin-top:50px;">
+                    <button class="btn btn-primary btn-previous" type="button" onClick="location.assign('https://t.me/sprintbank')"  style="padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
+                            <editor_block data-name='Previous 2' contenteditable="true">{{ __('Previous 2') }}</editor_block> @else {{ __('Previous 2') }} @endif
+                    </button>
+                    <button class="btn btn-primary btn-submit shake" id="next" onClick="location.assign('https://jivo.chat/xQE8U0bisX')" type="submit" data-bs-original-title="" title=""  style="margin-left:30px;padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
+                            <editor_block data-name='vnesti 2' contenteditable="true">{{ __('vnesti 2') }}</editor_block> @else {{ __('vnesti 2') }} @endif
+                    </button>
+                </div>
             </div>
           </div>
         </div>
