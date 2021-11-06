cd416b916e30cc2bc83c1b01d5f3cd663a482b1d
Serhii V <serhii.voronovych@gmail.com>
2021-11-06 15:25:29 +0800

    update

--- resources/views/accountPanel/replenishment/manual.blade.php
+++ resources/views/accountPanel/replenishment/manual.blade.php
@@ -11,18 +11,39 @@ Topup balance details
         <div class="col-sm-12">
           <div class="card">
             <div class="card-header">
-              <h5>@if(canEditLang() && checkRequestOnEdit())
+              <h5 style="text-transform:none;">@if(canEditLang() && checkRequestOnEdit())
                   <editor_block data-name='Replenishment details' contenteditable="true">{{ __('Replenishment details') }}</editor_block> @else {{ __('Replenishment details') }} @endif {{ $paymentSystem !== null ? $paymentSystem->name : '' }}</h5>
             </div>
             <div class="card-body" style="text-align: center; font-size:21px;">
-              <p>
-                @if(canEditLang() && checkRequestOnEdit())
-                  <editor_block data-name='Replenishment text 1' contenteditable="true">{{ __('Replenishment text 1') }}</editor_block> @else {{ __('Replenishment text 1') }} @endif
-              </p>
-              <h6>
-                @if(canEditLang() && checkRequestOnEdit())
-                  <editor_block data-name='Replenishment head 1' contenteditable="true">{{ __('Replenishment head 1') }}</editor_block> @else {{ __('Replenishment head 1') }} @endif
-              </h6>
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 1' contenteditable="true">{{ __('Replenishment text 1') }}</editor_block> @else {{ __('Replenishment text 1') }} @endif
+                </p>
+
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 2' contenteditable="true">{{ __('Replenishment text 2') }}</editor_block> @else {{ __('Replenishment text 2') }} @endif
+                </p>
+
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 3' contenteditable="true">{{ __('Replenishment text 3') }}</editor_block> @else {{ __('Replenishment text 3') }} @endif
+                </p>
+
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 4' contenteditable="true">{{ __('Replenishment text 4') }}</editor_block> @else {{ __('Replenishment text 4') }} @endif
+                </p>
+
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 5' contenteditable="true">{{ __('Replenishment text 5') }}</editor_block> @else {{ __('Replenishment text 5') }} @endif
+                </p>
+
+                <p>
+                    @if(canEditLang() && checkRequestOnEdit())
+                        <editor_block data-name='Replenishment text 6' contenteditable="true">{{ __('Replenishment text 6') }}</editor_block> @else {{ __('Replenishment text 6') }} @endif
+                </p>
 
                 <div class="f1-buttons" style="text-align: center;margin-top:50px;">
                     <button class="btn btn-primary btn-previous" type="button" onClick="location.assign('https://t.me/sprintbank')"  style="padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
