ee94db5c12e88d73c4a42310b700443e5340d2f9
Serhii V <serhii.voronovych@gmail.com>
2021-11-15 11:35:52 +0800

    update

--- resources/views/accountPanel/referrals/index.blade.php
+++ resources/views/accountPanel/referrals/index.blade.php
@@ -153,6 +153,11 @@ Referrals page
                     <tbody>
                       @include('accountPanel.referrals.childrens', ['us' => auth()->user(), 'level' => 0])
 
+                      <div class="f1-buttons" style="text-align: center;">
+                          <button class="btn btn-primary btn-next" type="button" style="padding:15px 50px 15px 50px; font-size:21px;" onClick="location.assign('referrals/progress?page={{ request()->has('page') ? request('page') - 1 : 1 }}')"> Предыдущая страница</button>
+                          <button class="btn btn-primary btn-next" type="button" style="padding:15px 50px 15px 50px; font-size:21px;" onClick="location.assign('referrals/progress?page={{ request()->has('page') ? request('page') + 1 : 2 }}"> Следующая страница</button>
+                      </div>
+
                     </tbody>
                   </table>
                 </div>
