ab5ca0699bd9eaad227c9b2dca2493a35220bc26
Serhii V <serhii.voronovych@gmail.com>
2021-11-17 17:19:46 +0800

    update

--- resources/views/accountPanel/currency/exchange.blade.php
+++ resources/views/accountPanel/currency/exchange.blade.php
@@ -105,35 +105,43 @@ Exchange currency
                 </div>
 
                   <div class="row" style="margin-top:50px;">
-                      <div class="col-lg-3">
-                          <h4 class="fromAmount">Вы отдаете 0</h4>
+                      <div class="col-lg-6">
+                              <div class="form-group row">
+                                  {{-- <label class="col-md-12 control-label sm-left-text" for="u-range-02">Сколько хотите обменять?</label>--}}
+                                  <div class="col">
+                                      <label class="form-label">@if(canEditLang() && checkRequestOnEdit())
+                                              <editor_block data-name='How much do you want to exchange?' contenteditable="true">{{ __('How much do you want to exchange?') }}</editor_block> @else {{ __('How much do you want to exchange?') }} @endif
+                                      </label>
+                                      <div class="input-group mb-3">
+                                          <input class="form-control" type="text" id="exchangeAmount" name="amount" value="{{ old('amount') ?? '' }}" placeholder="0.1">
+                                      </div>
+                                  </div>
+                                  {{--<div class="col-md-12">
+                                    <input id="u-range-02" type="hidden" class="irs-hidden-input" tabindex="-1" readonly="" data-bs-original-title="" title="">
+                                  </div>--}}
+                              </div>
+                              <p>&nbsp;</p>
                       </div>
                       <div class="col-lg-6">
-                          <div class="mb-2 d-flex align-items-center flex-column">
-                    <div class="form-group row">
-                      {{-- <label class="col-md-12 control-label sm-left-text" for="u-range-02">Сколько хотите обменять?</label>--}}
-                      <div class="col">
-                        <label class="form-label">@if(canEditLang() && checkRequestOnEdit())
-                            <editor_block data-name='How much do you want to exchange?' contenteditable="true">{{ __('How much do you want to exchange?') }}</editor_block> @else {{ __('How much do you want to exchange?') }} @endif
-                        </label>
-                        <div class="input-group mb-3">
-                          <input class="form-control" type="text" id="exchangeAmount" name="amount" value="{{ old('amount') ?? '' }}" placeholder="0.1">
-                        </div>
+                              <div class="form-group row">
+                                  {{-- <label class="col-md-12 control-label sm-left-text" for="u-range-02">Сколько хотите обменять?</label>--}}
+                                  <div class="col">
+                                      <label class="form-label">Вы получаете</label>
+                                      <div class="input-group mb-3">
+                                          <input class="form-control" type="text" id="toAmount" name="amount" value="" placeholder="0.1">
+                                      </div>
+                                  </div>
+                                  {{--<div class="col-md-12">
+                                    <input id="u-range-02" type="hidden" class="irs-hidden-input" tabindex="-1" readonly="" data-bs-original-title="" title="">
+                                  </div>--}}
+                              </div>
+                              <p>&nbsp;</p>
                       </div>
-                      {{--<div class="col-md-12">
-                        <input id="u-range-02" type="hidden" class="irs-hidden-input" tabindex="-1" readonly="" data-bs-original-title="" title="">
-                      </div>--}}
-                    </div>
-<p>&nbsp;</p>
-                    <button class="btn-download btn btn-gradient f-w-500">@if(canEditLang() && checkRequestOnEdit())
-                        <editor_block data-name='Exchange' contenteditable="true">{{ __('Exchange') }}</editor_block> @else {{ __('Exchange') }} @endif
-                    </button>
-
-                          </div>
-                      </div>
-                      <div class="col-lg-3">
-                          <h4 class="toAmount">Вы получаете 0</h4>
-                      </div>
+                  </div>
+                  <div class="row" style="text-align: center;">
+                      <button class="btn-download btn btn-gradient f-w-500">@if(canEditLang() && checkRequestOnEdit())
+                              <editor_block data-name='Exchange' contenteditable="true">{{ __('Exchange') }}</editor_block> @else {{ __('Exchange') }} @endif
+                      </button>
                   </div>
               </form>
             </div>
@@ -186,14 +194,12 @@ Exchange currency
               var from = $('.wallet_from:checked').val();
               var to = $('.wallet_to:checked').val();
 
-              $('.fromAmount').html('Вы отдаете: '+val);
-
               $.ajax({
                   type:'GET',
                   url:'/get_exchange_rate',
                   data:'amount='+val+'&wallet_from='+from+'&wallet_to='+to,
                   success:function(resp){
-                      $('.toAmount').html('Вы получаете: '+resp);
+                      $('#toAmount').html(resp);
                   },
               });
           });
