4adbb4a8d528b2d585e00af1b84668fb3e6af458
Serhii V <serhii.voronovych@gmail.com>
2021-11-18 13:28:18 +0800

    update

--- resources/views/accountPanel/dashboard.blade.php
+++ resources/views/accountPanel/dashboard.blade.php
@@ -651,7 +651,7 @@ Account
           data: [@foreach($withdraws_week as $item) {{ number_format($item, 2, '.', '') }}@if(!$loop->last), @endif @endforeach]
         }],
         chart: {
-          height: 300,
+          height: 350,
           type: 'area',
           toolbar: {
             show: false
