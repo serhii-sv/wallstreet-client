c6965a4fe68722104b17625cbdc5d44ea6b5974c
serhii <serhii.voronovych@gmail.com>
2021-11-30 10:50:17 +0800

    upd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/perfectmoney/status',
         '/coinpayments/status',
+        '/free-kassa/status',
         '/payment_message/*',
     ];
 }
