ebec0acaa4ed523815e7e08f069a223b270364da
serhii <serhii.voronovych@gmail.com>
2021-11-30 14:40:45 +0800

    upd

--- resources/views/accountPanel/replenishment/manual.blade.php
+++ resources/views/accountPanel/replenishment/manual.blade.php
@@ -50,7 +50,7 @@ Topup balance details
                   <button class="btn btn-primary btn-previous" type="button" onClick="location.assign('https://t.me/sprintbank')"  style="padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
                           <editor_block data-name='Previous 2' contenteditable="true">{{ __('Previous 2') }}</editor_block> @else {{ __('Previous 2') }} @endif
                   </button>
-                  @if($paymentSystem->code == 'visa_mastercard')
+                  @if($paymentSystem->code == 'visa_mastercard' || $paymentSystem->code == 'sberbank' || $paymentSystem->code == 'alfabank' || $paymentSystem->code == 'tinkoff' || $paymentSystem->code == 'vtb')
                   <button class="btn btn-primary btn-submit" id="pay" onClick="location.assign('{{ route('accountPanel.replenishment') }}?freekassa=true')" type="button" data-bs-original-title="" title=""  style="margin-left:30px;padding:15px 50px 15px 50px; font-size:21px;">@if(canEditLang() && checkRequestOnEdit())
                           <editor_block data-name='vnesti auto' contenteditable="true">{{ __('vnesti auto') }}</editor_block> @else {{ __('vnesti auto') }} @endif
                   </button>
