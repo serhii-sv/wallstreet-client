8265a0da1b2522dc15a125f342103803a7b32114
serhii <serhii.voronovych@gmail.com>
2021-12-20 11:07:09 +0800

    update

--- resources/views/layouts/app-header.blade.php
+++ resources/views/layouts/app-header.blade.php
@@ -4,7 +4,6 @@
         <!-- TradingView Widget BEGIN -->
         <div class="tradingview-widget-container">
             <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://ru.tradingview.com" rel="noopener" target="_blank"><span class="blue-text">Котировки</span></a> предоставлены TradingView</div>
             <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
                 {
                     "symbols": [
