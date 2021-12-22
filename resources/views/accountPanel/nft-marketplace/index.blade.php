1c5937e48c71e548f77457bb256690d8a38b6e35
serhii <serhii.voronovych@gmail.com>
2021-12-22 12:39:11 +0800

    update

--- resources/views/accountPanel/nft-marketplace/index.blade.php
+++ resources/views/accountPanel/nft-marketplace/index.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
   <div class="container-fluid" style="margin-top: 200px;">
-    <h1 class="text-center">@if(canEditLang() && checkRequestOnEdit()) <editor_block data-name='До старта NFT мркетплейса' contenteditable="true">{{ __('До старта NFT мркетплейса') }}</editor_block> @else {{ __('До старта NFT мркетплейса') }} @endif</h1>
+    <h1 class="text-center">@if(canEditLang() && checkRequestOnEdit()) <editor_block data-name='До старта NFT маркетплейса' contenteditable="true">{{ __('До старта NFT маркетплейса') }}</editor_block> @else {{ __('До старта NFT маркетплейса') }} @endif</h1>
     <div class="text-center mb-3" style="font-size: 32px;">
       @if(canEditLang() && checkRequestOnEdit()) <editor_block data-name='Осталось' contenteditable="true">{{ __('Осталось') }}</editor_block> @else {{ __('Осталось') }} @endif: <strong>{{ $diff_from_now_days }}</strong> @if(canEditLang() && checkRequestOnEdit()) <editor_block data-name='дней' contenteditable="true">{{ __('дней') }}</editor_block> @else {{ __('дней') }} @endif
     </div>
