b1c08a82789d01792d42b4183edd7c59b7646f6d
serhii <serhii.voronovych@gmail.com>
2021-12-23 20:28:38 +0800

    update

--- resources/views/accountPanel/ps/freekassa.blade.php
+++ resources/views/accountPanel/ps/freekassa.blade.php
@@ -1,7 +1,8 @@
-<form method='get' action='https://www.freekassa.ru/merchant/cash.php' id="payment" style="display:none;">
+<form method='get' action='https://pay.freekassa.ru/' id="payment" style="display:none;">
     <input type='hidden' name='m' value='<?=$merchantId?>'>
     <input type='hidden' name='oa' value='<?=$amount?>'>
     <input type='hidden' name='o' value='<?=$orderId?>'>
+    <input type='hidden' name='currency' value='RUB'>
     <input type='hidden' name='s' value='<?=$signature?>'>
     <input type='hidden' name='lang' value='ru'>
     <input type='hidden' name='us_login' value='<?=$user->login?>'>
