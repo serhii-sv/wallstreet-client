02ef3b13974569ae7c8e19a14a94fbb53d74ee61
serhii <serhii.voronovych@gmail.com>
2022-01-05 09:03:03 +0800

    update

--- resources/views/layouts/accountPanel/header.blade.php
+++ resources/views/layouts/accountPanel/header.blade.php
@@ -162,7 +162,7 @@
 
             @forelse($navbar_notifications as $item)
               <li class="notification" data-id="{{ $item->id }}" data-count="{{ $counts['notifications'] ?? 0 }}">
-                <p><i class="fa fa-circle-o me-3 font-success"> </i>{{ $item->notification->text }}
+                <p><i class="fa fa-circle-o me-3 font-success"> </i>{{ $item->notification->text ?? '' }}
                   <span class="pull-right">@if($item->notification->created_at){{ $item->notification->created_at->diffForHumans() }}@endif</span>
                 </p>
               </li>
