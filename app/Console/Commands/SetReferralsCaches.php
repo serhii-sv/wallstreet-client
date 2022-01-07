a1414e22c23f20e4859cb4ea69279e9251048668
serhii <serhii.voronovych@gmail.com>
2022-01-07 20:08:26 +0800

    update

--- app/Console/Commands/SetReferralsCaches.php
+++ app/Console/Commands/SetReferralsCaches.php
@@ -39,7 +39,7 @@ class SetReferralsCaches extends Command
     public function handle()
     {
         /** @var User $user */
-        foreach (User::orderBy('referrals_invested_total', 'desc')->get() as $user) {
+        foreach (User::orderBy('referrals_invested_total', 'asc')->get() as $user) {
             $this->info('work with user '.$user->login);
 
 //            cache()->forget('referrals.array.' . $user->id);
